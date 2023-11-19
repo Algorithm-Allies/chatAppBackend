5f062222cfb61018a6ec7c7f1e5e2716a609f541
mnicokar <nicokar.mobin@gmail.com>
2023-11-19 13:45:38 -0800

    changed the .get route to /getUsers

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -9,13 +9,7 @@ const {
 
 router.post("/", registerUser);
 router.post("/login", loginUser);
-router.get("/", getUsers);
+router.get("/getUsers", getUsers);
 router.get("/:id", getUser);
-
-// Simple test route
-router.get("/test", (req, res) => {
-  res.status(200).json({ message: "Server is working!" });
-});
-
 
 module.exports = router;
--- server.js
+++ server.js
@@ -18,6 +18,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/messages", require("./routes/messageRoutes"));
 
+
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
 });
