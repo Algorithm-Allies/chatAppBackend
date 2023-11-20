023f7e6917db089a1130834086f6897914e4f0e9
williamvelichko <williamvelichko2003@gmail.com>
2023-11-20 14:50:59 -0800

    change

--- server.js
+++ server.js
@@ -17,6 +17,9 @@ app.use(express.urlencoded({ extended: false }));
 // Routes
 app.use("/api/users", require("./routes/userRoutes"));
 app.use("/api/messages", require("./routes/messageRoutes"));
+app.get("/", (req, res) => {
+  res.send("Welcome to the root URL!");
+});
 
 app.listen(PORT, () => {
   console.log(`Server listening on port ${PORT}`);
