fd586a0c6b4ad034efb9dcff6785d7071a0cab83
mnicokar <nicokar.mobin@gmail.com>
2024-02-25 14:39:57 -0800

    adde cors

--- server.js
+++ server.js
@@ -11,7 +11,9 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors());
+app.use(cors({
+  origin: 'https://ripple-983hd6ber-mobins-projects.vercel.app',
+}));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
