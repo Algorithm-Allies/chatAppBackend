e5b1ba4ef46ba9cd186b1556395e520ddaff2ee5
mnicokar <nicokar.mobin@gmail.com>
2024-02-25 21:05:07 -0800

    removed cors requirement

--- server.js
+++ server.js
@@ -11,9 +11,7 @@ connectDB();
 const app = express();
 
 // Middleware
-app.use(cors({
-  origin: 'https://ripple-983hd6ber-mobins-projects.vercel.app',
-}));
+app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 
